82b6f3c3e703dfd620e6679cf3da5198ca10c1b0
otron <oddweb42+gith@gmail.com>
2012-10-27 20:45:14 +0200

    how typical. once I read the full specs of the assignment I realize some of the code has to be replaced because it won't work properly

--- 04-CSP/src/Board.java
+++ 04-CSP/src/Board.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * 
@@ -7,21 +8,57 @@ import java.util.ArrayList;
  */
 public class Board {
 	
-	int[] Q; //used to store the positions of placed queens
+	int[] Q; //used to store the positions of placed queens. 
+		//Q[i] = k --> i is the row#, k is the column#
 	int K; //the size of the board, coincidentally also the number of queens in an optimal solution.
 	StringBuilder debugInfo; //I have no idea if this will work.
 	int maxSteps; //part of the MIN-CONFLICTS algorithm
 	
 	
 	public Board(int K, int maxSteps) {
-		this.Q = new int[K];
-		for (int i = 0; i < K; i++)
-			this.Q[i] = -1; //hmm this idea of mine might've not been all that great.
+		this.K = K;
+		this.Q = new int[this.K];
+		this.maxSteps = maxSteps;
 		this.debugInfo = new StringBuilder();
+		
+		this.initialize();
+	}
+	
+	public void doSteps() {
+		int counter = 0;
+		Random rng = new Random();
+		while (counter < this.maxSteps && !this.isOptimal()) {
+			//First we select a random queen.
+			int q = rng.nextInt(this.K);
+			
+			//now we need to generate the number of conflicts for each cell in q's row.
+			
+			counter++;
+		}
 	}
 	
 	/**
 	 * 
+	 * @param q The row to find conflicts for
+	 * @return An array with the number of conflicts
+	 */
+	private int[] getConflictsForRow(int q) {
+		int[] res = new int[this.K];
+		
+		return null;
+	}
+	/**
+	 * Generates the initial board-state per the assignment's specifications
+	 */
+	private void initialize() {
+		Random rng = new Random();
+		//Randomly places one queen in each row.
+		for (int i = 0; i < this.K; i++)
+			this.Q[i] = rng.nextInt(this.K);
+	}
+	
+	/**
+	 * TODO: fix because this won't work
 	 * @return returns true if the board has a Queen in every column, false if it does not.
 	 */
 	public boolean isOptimal() {
