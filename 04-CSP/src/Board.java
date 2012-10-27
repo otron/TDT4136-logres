e05c758e400c9ff90db1da6c01c0bbe2c36efca5
otron <oddweb42+gith@gmail.com>
2012-10-27 17:56:54 +0200

    regular commits are good, right?

--- 04-CSP/src/Board.java
+++ 04-CSP/src/Board.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;
 
-
+/**
+ * 
+ * @author Odd
+ *
+ */
 public class Board {
 	
 	int[] Q; //used to store the positions of placed queens
 	int K; //the size of the board, coincidentally also the number of queens in an optimal solution.
+	StringBuilder debugInfo; //I have no idea if this will work.
+	int maxSteps; //part of the MIN-CONFLICTS algorithm
 	
 	
-	public Board(int K) {
+	public Board(int K, int maxSteps) {
 		this.Q = new int[K];
-		
+		for (int i = 0; i < K; i++)
+			this.Q[i] = -1; //hmm this idea of mine might've not been all that great.
+		this.debugInfo = new StringBuilder();
 	}
 	
 	/**
@@ -20,6 +28,10 @@ public class Board {
 		return (this.countQueens() == this.K ? true : false);
 	}
 	
+	/**
+	 * 
+	 * @return returns the number of queens currently in place on the board.
+	 */
 	public int countQueens() {
 		int res = 0;
 		for (int i : this.Q)
